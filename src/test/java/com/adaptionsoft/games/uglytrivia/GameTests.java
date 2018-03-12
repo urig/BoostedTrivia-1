d2fd210095cacd5a8c03d30abdc6bcc72337e5c8
Uri Goldstein <uri.goldstein@gmail.com>
2018-03-12 13:42:53 +0200

    First unit test (breaking compilation)

---
+++ src/test/java/com/adaptionsoft/games/uglytrivia/GameTests.java
@@ -0,0 +1,14 @@
+package com.adaptionsoft.games.uglytrivia;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameTests {
+
+	@Test
+	public void test() {
+		Game target = new Game();
+	}
+
+}
